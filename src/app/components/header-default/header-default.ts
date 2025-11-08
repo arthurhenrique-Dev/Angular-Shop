c1851605d9b65e05ecf6b678c4d65a0095081522
arthurhenrique-Dev <henriquearthur.dev@gmail.com>
2025-11-07 22:55:43 -0300

    o erro anterior era apenas de cache, sem necessidade de standalone

--- src/app/components/header-default/header-default.ts
+++ src/app/components/header-default/header-default.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header-default',
-  standalone: true,
   imports: [],
   templateUrl: './header-default.html',
   styleUrl: './header-default.css',
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -4,7 +4,6 @@ import { HeaderDefault } from '../header-default/header-default';
 @Component({
   selector: 'app-home',
   imports: [HeaderDefault],
-  standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
